31f563ff2b32189f144fef5d05e74c70eab79c57
Chris Beaver <chris@chrisbeaver.net>
2016-11-07 22:22:12 -0500

    Originals, thumbnails, md5 hashnames saving.

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Holding;
 use App\HoldingImage;
+use Image;
+use Storage;
 
 class ImageController extends Controller
 {
@@ -16,9 +18,28 @@ class ImageController extends Controller
 
     public function store(Request $request)
     {
-        // Store in storage/app/holding-images
-        $path = $request->image->store('holding-images');
+        // Store in storage/app/holding-images/originals
+        $request->image->store('holding-images/'.auth()->user()->id .'/originals/');
+        $file_name = md5_file($request->image).'.'.$request->image->extension();
+
+        // Fit image to site preferred size.
+        $img = Image::make($request->image);
+
+        // resize image
+        $img->resize(null, 768, function ($constraint) {
+            $constraint->aspectRatio();
+        });
+        // save image fit for site views
+        $img->save(storage_path('app/holding-images/'.auth()->user()->id.'/'.$file_name));
+
+        // Make thumbnail
+        $img->resize(null, 124, function ($constraint) {
+            $constraint->aspectRatio();
+        });
+
+        Storage::makeDirectory('holding-images/'.auth()->user()->id.'/thumbnails');
+        $img->save(storage_path('app/holding-images/'.auth()->user()->id.'/thumbnails/'.$file_name));
         
-        HoldingImage::create(['holding_id' => $request->holding_id, 'path' => $path]);
+       // HoldingImage::create(['holding_id' => $request->holding_id, 'path' => $path]);
     }
 }
